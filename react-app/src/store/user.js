6a12803ff6204f73a78a98ba8c330e61d712004b
Briana Robinson <briana.n.robinson@gmail.com>
2022-05-28 23:36:22 -0400

    started all users thunk for debug of edit

--- react-app/src/store/user.js
+++ react-app/src/store/user.js
@@ -1,6 +1,12 @@
+const GET_ALL_USERS = "user/getAllUsers";
 const GET_USER = "user/getUser";
 const EDIT_USER = "user/editUser";
 const DELETE_USER = "user/deleteUser";
+
+const getAllUsers = (users) => ({
+  type: GET_USER,
+  payload: users
+});
 
 const getUser = user => ({
   type: GET_USER,
@@ -16,6 +22,17 @@ const editUser = (user) => ({
   type: EDIT_USER,
   payload: user
 });
+
+// Get All Users
+export const getAllUsersThunk = () => async (dispatch) => {
+  const response = await fetch(`/api/users/`);
+  if (response.ok) {
+  
+    const users = await response.json();
+    dispatch(getAllUsers(users));
+    return users
+  }
+};
 
 // Get User
 export const getUserThunk = (userId) => async (dispatch) => {
@@ -54,6 +71,10 @@ const initialState = {}
 export default function userReducer(state = initialState, action) {
   let newState;
   switch (action.type) {
+    case GET_ALL_USERS:
+      newState = {...state}
+      newState.users = action.payload
+      return newState
     case GET_USER:
       newState = {...state}
       newState.user = action.payload
@@ -68,5 +89,3 @@ export default function userReducer(state = initialState, action) {
       return state;
   }
 }
-
-
