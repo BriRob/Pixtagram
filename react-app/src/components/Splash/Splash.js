6ac8767f3d3a4b646ba32af364c5eaa11e8687b2
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2022-05-31 23:38:25 -0400

    played around with splash page

--- react-app/src/components/Splash/Splash.js
+++ react-app/src/components/Splash/Splash.js
@@ -4,6 +4,7 @@ import { Redirect, useHistory, useParams } from "react-router-dom"
 import LoginForm from "../auth/LoginForm"
 import './Splash.css'
 import { getUserThunk } from "../../store/user"
+import { dotDotDotIcon } from "./SplashIcons"
 
 function Splash(){
 const dispatch = useDispatch()
@@ -16,17 +17,38 @@ useEffect(()=> {
 }, [dispatch])
 
 
-function goToProfile(){
-    history.push(`/users/${id}`)
-}
+// function goToProfile(){
+//     history.push(`/users/${id}`)
+// }
 
 if (!id) {
     return <Redirect to='/login' />;
 }
 return(
     <>
-    <h1>My Home Page</h1>
-    <button onClick={(e) => goToProfile(e)}>Go to profile</button>
+    <div className="home-page-body">
+    <div className="stories-container"></div>
+    <div className="feed">
+        <div className="post-card">
+            <div className="user-profile-info">
+                <img src="https://findingnatureblog.files.wordpress.com/2017/01/children-1822474_1280.jpg" style={{'height':'40px', 'width':'40px', 'border-radius':'50px'}}></img>
+                <div className="picture-info">
+                    <span style={{'color':'white', 'fontWeight':'bold'}}>User name</span>
+                </div>
+                <div className="dotdotdot">
+                {dotDotDotIcon}
+                </div>
+            </div>
+        <ul className="pictures">
+            <img className="feed-test" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/1200px-Shaqi_jrvej.jpg"></img>
+            <img className="feed-test" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/24701-nature-natural-beauty.jpg/1280px-24701-nature-natural-beauty.jpg"></img>
+            <img className="feed-test" src="https://cdn-prod.medicalnewstoday.com/content/images/articles/325/325466/man-walking-dog.jpg"></img>
+            <img className="feed-test" src="https://cdn.pixabay.com/photo/2014/02/27/16/10/tree-276014__340.jpg"></img>
+        </ul>
+        </div>
+    </div>
+    {/* <button onClick={(e) => goToProfile(e)}>Go to profile</button> */}
+    </div>
     </>
 )
 
