f2e8bf5b57d764e491493834232656655f0617ab
Briana Robinson <briana.n.robinson@gmail.com>
2022-06-02 00:39:55 -0400

    working on CSS editUser

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import LogoutButton from "../auth/LogoutButton";
 import CreatePost from "../PostPages/CreatePost";
 import "./index.css";
@@ -26,13 +26,18 @@ import ProfileButton from "./ProfileButton";
 import { getUserThunk } from "../../store/user";
 
 const NavBar = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  // const location = useLocation()
+  const {pathname} = useLocation()
   const user = useSelector((state) => state.session.user);
   const profile = useSelector(
     (state) => state?.userReducer?.user?.profile_pic_url
   );
 
   const [showModal, setShowModal] = useState(false);
+  console.log("location pathname \n\n", pathname)
+
+
 
   const openModal = () => {
     if (showModal) return;
@@ -40,7 +45,7 @@ const NavBar = () => {
   };
 
   useEffect(() => {
-    dispatch(getUserThunk(user.id))
+    dispatch(getUserThunk(user.id));
     if (!showModal) return;
 
     const closeModal = () => {
@@ -51,6 +56,10 @@ const NavBar = () => {
     // modal.addEventListener('click', (e) => {
     //   e.target.('showModal')
     // })
+    if (pathname !== "/") {
+      // setHouseColor(darkModeHomeIcon);
+      removeIconColor()
+    }
 
     return () => document.removeEventListener("click", closeModal);
   }, [showModal]);
@@ -67,6 +76,9 @@ const NavBar = () => {
   const [heartIconColor, setHeartIconColor] = useState(darkModeHeartHomeIcon);
 
   const [valueState, setValueState] = useState("");
+
+
+
   const logo = (
     <img
       src="https://fontmeme.com/permalink/220601/86a21de467499ff0a91e214d1a326624.png"
@@ -100,7 +112,7 @@ const NavBar = () => {
     e.stopPropagation();
     removeIconColor();
     setPostIconColor(darkModeFilledInPostIcon);
-    openModal()
+    openModal();
     // return (
     //   <CreatePost boolean={true}/>
     // )
@@ -123,99 +135,76 @@ const NavBar = () => {
   };
 
   return (
-    <nav>
-      <div className="nav-bar">
-        <div id="nav-bar-container">
-          <div>
-            <NavLink to="/" exact={true}>
-              {logo}
-            </NavLink>
-          </div>
-          <div className="_aawf_aawg">
-            <input
-              aria-label="Search input"
-              autoCapitalize="none"
-              className="search-bar"
-              // color={searchField === 'Search' ? "grey" : "white"}
 
-              value={""}
-              placeholder="Search"
-              type="text"
-              readOnly
-            />
-            <div className="_aaw8" role="button" tabIndex="0">
-              <div className="_aaw9">{darkModeSearchIcon}</div>
-              <span className="search-text"></span>
-            </div>
-          </div>
-          <div className="icons">
-            <NavLink
-              to="/"
-              exact={true}
-              id="house-icon-id"
-              onClick={(e) => fillInHouse(e)}
-            >
-              {houseColor}
-            </NavLink>
-            <NavLink
-              to="/"
-              exact={true}
-              id="post-icon-id"
-              onClick={(e) => fillInPost(e)}
-            >
-              {postIconColor}
-            </NavLink>
-            <NavLink
-              to="/"
-              exact={true}
-              id="explore-icon-id"
-              onClick={(e) => fillInExplore(e)}
-            >
-              {exploreIconColor}
-            </NavLink>
-            <NavLink
-              to="/"
-              exact={true}
-              id="heart-icon-nav-id"
-              onClick={(e) => fillInNavHeart(e)}
-            >
-              {heartIconColor}
-            </NavLink>
-            <ProfileButton user={user} profile={profile}/>
+    <div className="nav-bar">
+      <div id="nav-bar-container">
+        <div>
+          <NavLink to="/" exact={true}>
+            {logo}
+          </NavLink>
+        </div>
+        <div className="_aawf_aawg">
+          <input
+            aria-label="Search input"
+            autoCapitalize="none"
+            className="search-bar"
+            // color={searchField === 'Search' ? "grey" : "white"}
+
+            value={""}
+            placeholder="Search"
+            type="text"
+            readOnly
+          />
+          <div className="_aaw8" role="button" tabIndex="0">
+            <div className="_aaw9">{darkModeSearchIcon}</div>
+            <span className="search-text"></span>
           </div>
         </div>
-            <div>
-              {showModal && (
-                <CreatePost hideModal={() => setShowModal(false)} changePostIcon={() => setPostIconColor(darkModePostIcon)}/>
-              )}
-            </div>
+        <div className="icons">
+          <NavLink
+            to="/"
+            exact={true}
+            id="house-icon-id"
+            onClick={(e) => fillInHouse(e)}
+          >
+            {houseColor}
+          </NavLink>
+          <NavLink
+            to="/"
+            exact={true}
+            id="post-icon-id"
+            onClick={(e) => fillInPost(e)}
+          >
+            {postIconColor}
+          </NavLink>
+          <NavLink
+            to="/"
+            exact={true}
+            id="explore-icon-id"
+            onClick={(e) => fillInExplore(e)}
+          >
+            {exploreIconColor}
+          </NavLink>
+          <NavLink
+            to="/"
+            exact={true}
+            id="heart-icon-nav-id"
+            onClick={(e) => fillInNavHeart(e)}
+          >
+            {heartIconColor}
+          </NavLink>
+          <ProfileButton user={user} profile={profile} />
+        </div>
       </div>
-      {/* <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul> */}
-    </nav>
+      <div>
+        {showModal && (
+          <CreatePost
+            hideModal={() => setShowModal(false)}
+            changePostIcon={() => setPostIconColor(darkModePostIcon)}
+          />
+        )}
+      </div>
+    </div>
   );
 };
 
