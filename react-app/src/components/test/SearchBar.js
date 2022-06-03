382f00868e3d210d08d89de41713d74f68725b1c
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2022-06-03 00:24:53 -0400

    Newbugfixes (#71)
    * updates profile pic in nav bar when you submit
    
    * styling for nav bar

--- react-app/src/components/test/SearchBar.js
+++ react-app/src/components/test/SearchBar.js
@@ -1,10 +1,18 @@
-import React from "react"
-import './searchbar.css'
-function SearchBar() {
-    return (
-    <>
-        </>
-    )
-}
+// import React from "react"
+// import './searchbar.css'
+// import { darkModeSearchIcon } from "../NavBar/Navicons"
+// function SearchBar() {
+//     const icon = darkModeSearchIcon
+//     return (
+//         <>
+//         <form className="nosubmit">
+//             <input className="nosubmit"
+//             type="search"
+//             placeholder="Search..."
+//             style={{'backgroundImage': `url(${icon})`}}></input>
+//         </form>
+//         </>
+//     )
+// }
 
-export default SearchBar
+// export default SearchBar
